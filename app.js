5a12215e79f6ec2df5fcfad6ed1226ffedb4f541
ans <ansbilalabc@gmail.com>
2020-11-08 19:52:03 +0500

    add subs api completed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 var subsRouter = require('./routes/subs');
 var config = require('./config');
 var mysql = require('mysql');
-
+var errorHandler = require('./middle-wares/error-handler')
 var app = express();
 
 
@@ -48,20 +48,9 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public') + '/index.html');
 })
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(errorHandler);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 app.listen(3000 || process.env.PORT, () => {
   console.log("Server listening on port 3000")
 });
--- routes/subs.js
+++ routes/subs.js
@@ -4,18 +4,51 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/add', function(req, res) {
   
+    console.log(req.body)
   let firstName = req.body.firstName;
   let lastName = req.body.lastName;
   let email = req.body.email;
   let phone = req.body.phone;
+  let personalCreditReport = req.body.personalCreditReport;
+  let goalWithCredit = req.body.goalWithCredit;
+  let doItYourself = req.body.doItYourself;
+  let businessCard = req.body.businessCard;
+  let businessCreditCheck = req.body.businessCreditCheck
 
-  let query = "INSERT INTO `subscriber` (FirstName, LastName, PhoneNumber,Email) VALUES('"+firstName+"','"+lastName+"','"+email+"','"+phone+"')";
-  db.query(query, (err, result) => {
-    if (err) {
+  let checkQuery = "SELECT COUNT(Id) as count FROM subscriber WHERE Email = '"+email+"' or PhoneNumber = '"+phone+"'"
+
+  db.query(checkQuery, (err,result)=>{
+      console.log(result);
+    if(err)
         return res.status(500).send(err);
+    if(result && result[0].count > 0)
+        return res.status(400).send('User Already Exist')
+    else
+    {
+        let insertSubQuery = "INSERT INTO `subscriber` (FirstName, LastName, PhoneNumber,Email) VALUES('"+firstName+"','"+lastName+"','"+phone+"','"+email+"')";
+        db.query(insertSubQuery, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            else
+            {
+                let insertAQuery = "INSERT INTO `subscriber_answer`(`SubId`, `PersonalCreditReport`, `GoalWithCredit`, `DoItYourself`, `BusinessCard`, `BusinessCreditCheck`) \
+                                    SELECT subscriber.Id , '"+personalCreditReport+"', '"+goalWithCredit+"','"+doItYourself+"','"+businessCard+"','"+businessCreditCheck+"' \
+                                    FROM subscriber WHERE subscriber.Email = '"+email+"' AND subscriber.PhoneNumber = '"+phone+"'";
+                db.query(insertAQuery,(err,result)=>
+                {
+                    if(err){
+                        return res.status(500).send(err);
+                        
+                    }
+                    res.sendStatus(200);
+                });
+            }
+            
+        });
     }
-    res.sendStatus(200);
-});
+  });
+
 
 });
 
